bcb3bc8abc47b77050bbe47ae75af8d9068daf0a
Andy Lopez <ALopez@ucvts.tec.nj.us>
2019-11-15 10:20:09 -0500

    uhhh k.

--- src/sum.js
+++ src/sum.js
@@ -3,10 +3,20 @@ const readlineSync = require("readline-sync");
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = Number.MIN_SAFE_INTEGER;
 
-let lowerbound = Number(readlineSync.question("\nLower bound: "));
-let upperbound = Number(readlineSync.question("\nUpper bound: "));
+let lowerbound = 1;
+let upperbound = -1;
 
-while (lowerbound>upperbound) {
+while (lowerbound > upperbound || Number.isNaN(lowerbound) || Number.isNaN(upperbound) || (lowerbound < MIN || lowerbound > MAX) || (upperbound < MIN || upperbound > MAX)) {
   lowerbound = Number(readlineSync.question("\nLower bound: "));
-  upperbound = Number(readlineSync.question("\nUpper bound: "));
+  upperbound = Number(readlineSync.question("Upper bound: "));
 }
+
+let sum = 0;
+
+for (let i = lowerbound; i <= upperbound; i++) {
+    if (i % 2 === 0) {
+      sum = (sum + i);
+    }
+}
+
+console.log("\n" + sum + "." )
