e9b57524e123fe76abe3cfdb2b183561a6928ec0
Andy Lopez <andy@Andys-MacBook-Air.local>
2019-11-22 21:06:17 -0500

    reverse and sum done

--- src/reverse.js
+++ src/reverse.js
@@ -3,9 +3,30 @@ const readlineSync = require("readline-sync");
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = 1;
 
-let integer = Number(readlineSync.question("\nPositive integer: "));
+let integer = 0
 
-while ( Number.isNaN(integer) || Number.isNaN(integer) || (integer < MIN || integer > MAX) || (integer < MIN || integer > MAX)) {
-  integer = Number(readlineSync.question("\nPositive integer: "));
+console.log()
+
+do {
+  integer = Number(readlineSync.question("\rPositive integer: "));
+}
+while ((integer < MIN || integer > MAX) || Number.isNaN(integer) || !Number.isInteger(integer) ) {
 
 }
+
+ let space = "";
+
+while (integer > 0) {
+    let reverse = (integer % 10);
+    integer = Math.floor(integer / 10);
+
+
+if (integer>0){
+  space = space + reverse + ", ";
+    } else {
+      space = space + reverse + ". ";
+    }
+  }
+
+
+console.log("\n" + space + "\n");
